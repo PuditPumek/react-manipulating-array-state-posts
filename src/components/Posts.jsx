de22b0e69fe4cf4efcd6d59e68fca84b42dedf08
Pumek <mekawinlnwza@gmail.com>
2026-04-15 17:45:17 +0700

    Complete

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,28 +1,52 @@
+import React from "react";
+import { postData } from "../raw-data/post-data";
+
 function Posts() {
+
+  const [posts, setPosts] = React.useState(postData);
+  const [sortBy, setSortBy] = React.useState("id");
+
+  const handleLike = (postId) => {
+    setPosts(posts.map((post) => {
+      if (post.id === postId) {
+        return {...post, likes: post.likes + 1};
+      }
+      return post;
+    }));
+  };
+
+  const handleDislike = (postId) => {
+    setPosts(posts.map((post) => {
+      if (post.id === postId) {
+        return {...post, likes: post.likes - 1};
+      }
+      return post;
+    }));
+  };
+
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
       <div class="post-list">
-        <div class="post-item">
+        {posts.map((post) => (
+          <div class="post-item" key={post.id}>
+
           <div class="post-header">
-            <h2>Post Title #1</h2>
+            <h2>{post.title}</h2>
             <div class="post-social-media-stats">
               <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+              <span class="post-likes">{post.likes}</span>
             </div>
           </div>
           <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
+            {post.content}
           </p>
           <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
+            <button class="like-button" onClick={() => handleLike(post.id)}>Like</button>
+            <button class="dislike-button" onClick={() => handleDislike(post.id)}>Dislike</button>
           </div>
         </div>
+        ))}
       </div>
     </div>
   );
